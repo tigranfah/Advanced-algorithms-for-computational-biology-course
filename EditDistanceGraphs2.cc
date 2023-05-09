98a73647286a24f3c653fccccd2590577db3e1c6
tigransthegreat <tigran.fahradyan01@gmail.com>
2023-05-09 23:51:02 +0400

    fix seed heuristic

--- EditDistanceGraphs2.cc
+++ EditDistanceGraphs2.cc
@@ -3,7 +3,7 @@
 #include <functional>
 #include <queue>
 #include <cmath>
-#include <unordered_map>
+#include <map>
 #include <unordered_set>
 
 using namespace std;
@@ -24,13 +24,13 @@ struct position {
     }
 };
 
-template<>
-struct hash<position> {
-    size_t operator()(const position& pos) const {
-        // TODO: Bad hashing function, change it
-        return pos.x < pos.y ? pos.y * pos.y + pos.x : pos.x * pos.x + pos.x + pos.y;
-    }
-};
+// template<>
+// struct hash<position> {
+//     size_t operator()(const position& pos) const {
+//         // TODO: Bad hashing function, change it
+//         return pos.x < pos.y ? pos.y * pos.y + pos.x : pos.x * pos.x + pos.x + pos.y;
+//     }
+// };
 
 /* entry struct that shows the cost of going to the node in x, y position with cost weight */
 struct entry {
@@ -38,8 +38,7 @@ struct entry {
     ll g_value, h_value;
 
     bool operator>(const entry& o) const {
-        if (g_value + h_value != o.g_value + o.h_value) return g_value + h_value > o.g_value + o.h_value;
-        return g_value > o.g_value;
+        return g_value + h_value > o.g_value + o.h_value;
     }
 };
 
@@ -53,11 +52,10 @@ struct node {
     position pos;
     ll value;
     vector<edge> adj;
-    bool used;
 
     node() = default;
 
-    node(const position& pos, ll v) : pos(pos), value(v), adj(vector<edge>()), used(false) { }
+    node(const position& pos, ll v) : pos(pos), value(v), adj(vector<edge>()) { }
 };
 
 vector<string> read_input() {
@@ -74,7 +72,7 @@ vector<string> read_input() {
 	return tokens;
 }
 
-unordered_map<position, node> position_to_node;
+map<position, node> position_to_node;
 vector<ll> suffix_non_matches_along_x, suffix_non_matches_along_y;
 string v, w;
 int n, m;
@@ -86,6 +84,8 @@ inline ll h_function(const position& pos) {
     
     if (pos.y + 1 < n)
         value = max(value, suffix_non_matches_along_y[pos.y + 1]);
+
+    // value = max(value, (ll) abs(pos.x - pos.y));
         
     return value;
 }
@@ -95,17 +95,20 @@ vector<ll> compute_suffix_non_matches(const string& str1, const string& str2, in
     unordered_set<string> kmers;
     // generate all kmers of v
     for (int i = 0; i < len1; ++i) {
-        for (int j = i + 1; j <= len1 - i; ++j) {
+        for (int j = 1; j <= len1 - i; ++j) {
             kmers.insert(str1.substr(i, j));
+            // cout << str1.substr(i, j) << "\n";
         }
     }
 
     vector<ll> non_matches(len2, 0), suffix_non_matches(len2, 0);
     for (int i = 0; i <= len2 - k; i += k) {
-        // cout << w.substr(i, k) << "\n";
-        if (kmers.find(str2.substr(i, k)) == kmers.end())
+        // cout << "checking " << str2.substr(i, k) << " ";
+        if (kmers.find(str2.substr(i, k)) == kmers.end()) {
             non_matches[i] = 1;
-
+            // cout << "not found";
+        }
+        // cout << "\n";
     }
     suffix_non_matches[len2 - 1] = 0;
     for (int i = len2 - 2; i >= 0; --i) {
@@ -127,7 +130,6 @@ inline node* get_node(const position& pos) {
 inline void explore_node(const position& pos) {
     if (get_node(pos) != nullptr) return;
 
-    // cout << "expanding " << pos.y << " " << pos.x << "\n";
     node node_(pos, INF);
 
     if (pos.y < n - 1)
@@ -144,10 +146,10 @@ inline void explore_node(const position& pos) {
 
 void find_shortest_path(const position& from, const position& to, function<ll(const position&)> heuristic) {
 
-    /* implementation of Dijkstra algorithm for finding the shortest path from starting to target position */
+    /* implementation of A* algorithm for finding the shortest path from starting to target position */
     priority_queue<entry, vector<entry>, greater<entry>> pq;
     explore_node(from);
-    pq.push({from, 0, h_function(position {0, 0})});
+    pq.push({from, 0, heuristic(position {0, 0})});
 
     while (!pq.empty()) {
 
@@ -155,22 +157,19 @@ void find_shortest_path(const position& from, const position& to, function<ll(co
         pq.pop();
         assert(e.pos.x < m and e.pos.y < n);
         // cout << "popped " << e.pos.y << " " << e.pos.x << " " << e.weight << "\n";
-        // cout << "popped " << e.pos.y << " " <<   e.pos.x << " g:" << e.g_value << ", h:" << e.h_value << "\n";
+        // cout << "popped " << e.pos.y << " " << e.pos.x << " g:" << e.g_value << ", h:" << e.h_value << "\n";
         node& node_ = *get_node(e.pos);
-        if (node_.used) continue;
-        node_.used = true;
+        if (e.g_value >= node_.value) continue;
         node_.value = e.g_value;
 
         if (e.pos == to)
             break;
 
         for (const edge& p : node_.adj) {
-            // cout << "passing with " << p.pos.y << " " << p.pos.x << " "  "\n";
+            // cout << "insert " << p.pos.y << " " << p.pos.x << ", g:" << e.g_value + p.weight << ", h:" << heuristic(p.pos) << "\n";
             explore_node(p.pos);
-            if (!get_node(p.pos)->used) {
-                // cout << "inserting " << p.pos.y << " " << p.pos.x << " g:" << p.weight + e.g_value << ", h:" << h_function(p.pos) << "\n";
-                pq.push({p.pos, e.g_value + p.weight, heuristic(p.pos)});
-            }
+            pq.push({p.pos, e.g_value + p.weight, heuristic(p.pos)});
+            // }
         }
     }
 }
@@ -181,23 +180,10 @@ void align_with_heuristic() {
 
     cout << v << " " << w << "\n";
 
-    int k = max(2, (int) log(m));
-
-    suffix_non_matches_along_x = compute_suffix_non_matches(v, w, k);
-    suffix_non_matches_along_y = compute_suffix_non_matches(w, v, k);
+    suffix_non_matches_along_x = compute_suffix_non_matches(v, w, max(1, (int) log(m)));
+    suffix_non_matches_along_y = compute_suffix_non_matches(w, v, max(1, (int) log(n)));
 
     find_shortest_path(position {0, 0}, position {n - 1, m - 1}, h_function);
-
-    // cout << get_node(position {n - 1, m - 1})->value << "\n";
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            const node* node_ = get_node(position {i, j});
-            if (node_ && node_->value != INF) cout << node_->value << " ";
-            else cout << "- ";
-        }
-        cout << "\n";
-    }
 }
 
 void align_without_heuristic() {
@@ -207,17 +193,6 @@ void align_without_heuristic() {
     cout << v << " " << w << "\n";
 
     find_shortest_path(position {0, 0}, position {n - 1, m - 1}, [] (const position& pos) { return 0; } );
-
-    // cout << get_node(position {n - 1, m - 1})->value << "\n";
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            const node* node_ = get_node(position {i, j});
-            if (node_ && node_->value != INF) cout << node_->value << " ";
-            else cout << "- ";
-        }
-        cout << "\n";
-    }
 }
 
 int main() {
@@ -237,7 +212,26 @@ int main() {
     m = (int) w.size();
 
     align_with_heuristic();
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            const node* node_ = get_node(position {i, j});
+            if (node_ && node_->value != INF) cout << node_->value << " ";
+            else cout << "- ";
+        }
+        cout << "\n";
+    }
+
     align_without_heuristic();
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            const node* node_ = get_node(position {i, j});
+            if (node_ && node_->value != INF) cout << node_->value << " ";
+            else cout << "- ";
+        }
+        cout << "\n";
+    }
 
     return 0;
 }
